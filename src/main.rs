4a965a622cad9bb5f5cf9ef513641081fd7a8899
Eric Douglass <dougli1sqrd+git@gmail.com>
2022-04-29 17:38:29 -0700

    the rest of the timer interrupt changes

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 #![no_std]
 #![no_main]
 
-use esp32c3_hal::{gpio::IO, pac::Peripherals, prelude::*, Delay, RtcCntl, Timer, Serial};
+use esp32c3_hal::{gpio::IO, pac::Peripherals, prelude::*, Delay, RtcCntl, Timer};
+use esp_hal_common::interrupt;
 use panic_halt as _;
 use riscv_rt::entry;
-
-use core::fmt::Write;
 
 use vgaterm;
 use vgaterm::sprintln;
@@ -17,17 +16,26 @@ fn main() -> ! {
     // Disable the watchdog timers. For the ESP32-C3, this includes the Super WDT,
     // the RTC WDT, and the TIMG WDTs.
     let mut rtc_cntl = RtcCntl::new(peripherals.RTC_CNTL);
-    let mut timer0 = Timer::new(peripherals.TIMG0);
     let mut timer1 = Timer::new(peripherals.TIMG1);
-    // let mut serial0 = Serial::new(peripherals.UART0).unwrap();
-
-    vgaterm::configure(peripherals.UART0);
-
+    
     rtc_cntl.set_super_wdt_enable(false);
     rtc_cntl.set_wdt_enable(false);
-    timer0.disable();
     timer1.disable();
+    
+    vgaterm::configure(peripherals.UART0);
+    vgaterm::configure_timer0(peripherals.TIMG0);
 
+    vgaterm::enable_timer0_interrupt(
+        &interrupt::CpuInterrupt::Interrupt1, 
+        interrupt::Priority::Priority1
+    );
+
+    vgaterm::start_timer0(10_000_000);
+
+    unsafe {
+        riscv::interrupt::enable();
+    }
+    
     // Set GPIO5 as an output, and set its state high initially.
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
     let mut led = io.pins.gpio5.into_push_pull_output();
@@ -45,5 +53,15 @@ fn main() -> ! {
         led.toggle().unwrap();
         delay.delay_ms(500u32);
     }
+}
+
+#[no_mangle]
+pub fn interrupt1() {
+    riscv::interrupt::free(|_| {
+        vgaterm::clear_timer0(interrupt::CpuInterrupt::Interrupt1);
+        sprintln!("Interrupt 1");
+        
+        vgaterm::start_timer0(10_000_000);
+    });
 }
 
